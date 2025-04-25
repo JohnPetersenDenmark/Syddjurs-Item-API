44c8a1ff98c76ba85c93e16f9c0c7efd8725c195
ckes <ckes@WS106327B>
2025-04-25 07:16:23 +0200

    ok

--- Syddjurs Item API/Models/Item.cs
+++ Syddjurs Item API/Models/Item.cs
@@ -10,7 +10,7 @@ namespace Syddjurs_Item_API.Models
 
         public string? Color { get; set; }
 
-        public string? Description { get; set; }
+        public string? Description { get; set; } 
 
         public string? Sex { get; set; }
 
