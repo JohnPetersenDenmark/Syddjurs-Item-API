2b58950590daf404a89b070d747a7741a3b77a6a
ringstedstamps <stampringstedogomegn@gmail.com>
2025-04-30 02:01:30 +0200

    OK

--- Syddjurs Item API/Models/RegisterDto.cs
+++ Syddjurs Item API/Models/RegisterDto.cs
@@ -1,9 +1,16 @@
-﻿namespace Syddjurs_Item_API.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Syddjurs_Item_API.Models
 {
     public class RegisterDto
     {
-        public string UserName { get; set; }     // or Email
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("email")]
         public string Email { get; set; }
+
+        [JsonPropertyName("password")]
         public string Password { get; set; }
     }
 }
